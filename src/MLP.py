5583416f9f2d838fdfff2bb63f26282850631766
MateuszMalowiecki <33604498+MateuszMalowiecki@users.noreply.github.com>
2023-04-09 10:45:05 +0000

    challenge

--- src/MLP.py
+++ src/MLP.py
@@ -9,10 +9,18 @@ class Perceptron:
 
     def __init__(self, inputs, bias=1.0):
         """Return a new Perceptron object with the specified number of inputs (+1 for the bias)."""
-        self.weights = (np.random.rand(inputs + 1) * 2) - 1
+        self.weights = (np.random.rand(inputs+1) * 2) - 1
         self.bias = bias
 
     def run(self, x):
         """Run the perceptron. x is a python list with the input values."""
-        x_sum=np.dot(np.append(x, self.bias), self.weights)
+        x_sum = np.dot(np.append(x, self.bias), self.weights)
         return self.sigmoid(x_sum)
+
+# Challenge: Finish the following methods:
+
+    def set_weights(self, w_init):
+        self.weights = np.array(w_init)
+
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
